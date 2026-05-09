643e01f41266ffc98121473d8adaa4f4b6d861cd
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2026-05-09 15:31:58 +0300

    done database

--- programmistic/users/admin.py
+++ programmistic/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile
+from posts.models import Post
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'avatar', 'post']
+    list_filter = ['user', 'bio', 'avatar', 'post']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'user']
+    list_filter = ['title', 'content', 'user']
