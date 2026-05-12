058dae8ff0742a887106727c6397aae5910542f0
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2026-05-12 19:33:24 +0300

    fixed

--- programmistic/users/models.py
+++ programmistic/users/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.models import Post
-# Create your models here.
+
+
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    bio = models.TextField(null=True, blank=True)
-    avatar = models.ImageField(upload_to='avatars', null=True, blank=True)  
-    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    bio = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
