e8cc9de16be5b46feb7354227814e7e75036aea1
OccupationalYou <dydd32008@gmail.com>
2026-05-13 18:40:47 +0300

    Registation
    Updated posts/models/py, users/models.py, programmistic/settings.py, programmistic/urls.py.
    Added users/forms.py, users/urls.py, users/views.py, base.html, dashboard.html, login.html, profile.html, register.html.

---
+++ programmistic/users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/', views.profile, name='profile'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
